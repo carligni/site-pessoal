63db18f33711df777c5d6ee49181f863eb937c9e
kratos <kratos@vodafone.pt>
2015-05-14 17:58:47 +0100

    delete views

--- resources/views/songs/lista.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('master')
-
-@section('content')
-    @foreach($cancoes as $cancao)	
-    	<p><a href="/songs/{{ $cancao->slug }}">{{ $cancao->title }}</a></p>
-    @endforeach
-    
-    {{ $letra }}
-@endsection
